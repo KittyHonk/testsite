b3f05c73686c70d35577257e6bc1e4292011a3b8
KittyHonk <nikitosmail@yandex.ru>
2022-04-14 18:41:01 +0300

    ***

--- server/controllers/tableController.js
+++ server/controllers/tableController.js
@@ -3,14 +3,18 @@ const ApiError = require('../error/ApiError')
 
 class tableController {
     async load(req, res) {
-
+        const tables = await Table.findAll({
+            attributes: ['value_id', 'value', 'result_id', 'result'],
+            where: {
+                table_name: "Молоко СХП"
+            }
+        })
+        return res.json(tables)
     }
 
     async create(req, res) {
-        const {table_name} = req.body
-        const {value_id} = req.body
-        const {result_id} = req.body
-        const name = await Table.create({table_name, value_id, result_id})
+        const {table_name, value_id, value, result_id, result} = req.body
+        const name = await Table.create({table_name, value_id, value, result_id, result})
         return res.json(name)
     }
 }
