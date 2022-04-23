7b8283e1b55763b5db0106616147c278eb36792e
KittyHonk <nikitosmail@yandex.ru>
2022-04-23 20:39:11 +0300

    ***

--- client/src/pages/Tables.js
+++ client/src/pages/Tables.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import MilkSHP from "../component/MilkSHP/MilkSHP";
+import {useParams} from "react-router-dom";
+import MilkKFH from "../component/MilkKFH/MilkKFH";
+import ForageHarvest from "../component/ForageHarvest/ForageHarvest";
+import CornSilage from "../component/CornSilage/CornSilage";
 
 const Tables = observer(() => {
-    const regionList = [{name: "Анна"}, {name: "Бобров"}, {name: "Борисоглебск"}]
 
-    return (
-        <MilkSHP rowName={regionList}/>
-    );
+    const params = useParams()
+    const regionList = [
+        {name: "Анна"},
+        {name: "Бобров"},
+        {name: "Борисоглебск"},
+        {name: "Воронеж"},
+        {name: "Бутурлиновка"}
+    ]
+
+    switch(params.table_name) {
+        case "Молоко СХП": {
+            return <MilkSHP rowName={regionList}/>
+        }
+        case "Молоко КФХ" : {
+            return <MilkKFH rowName={regionList}/>
+        }
+        case "Заготовка кормов" : {
+            return <ForageHarvest rowName={regionList}/>
+        }
+        case "Кукуруза на силос": {
+            return <CornSilage rowName={regionList}/>
+        }
+        default: {
+            console.log(params.table_name)
+        }
+    }
 });
 
 export default Tables;
